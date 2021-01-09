53f0c17c1732104e24842fa70a8894fdceba157a
sjlouji <sjlouji10@gmail.com>
2021-01-09 14:23:07 +0530

    test

--- api/Nextbooks/app/controller/books/auth.controller.js
+++ api/Nextbooks/app/controller/books/auth.controller.js
@@ -33,7 +33,6 @@ module.exports = {
     // Passport Login
     passportLogin : async function(req, res){
         const { email, password } = req.body;
-        console.log('added')
         // Validating Payload with JOI
         const {validate} =  loginValidation(req.body);
         if(validate) return res.status(400).json({"error":validate.error.details[0].message});
